7616ae652692db8e38b2c2e3d670e66e246f3173
nphekt <m.r.lof@tele2.nl>
2013-06-24 21:01:05 +0200

    Update hodor.js

--- hodor.js
+++ hodor.js
@@ -7,10 +7,10 @@ function(hodor) IsValidHodor{
 	if (hodor == 'hodor?') return 1; 		// -
 	if (hodor == 'hodoor') return 1; 		// >
 	if (hodor == 'hod-or') return 1; 		// <
-	if (hodor == 'ho-eh-dor') return 1; 	// [
-	if (hodor == 'ho-ah-dor') return 1; 	// ]
+	if (hodor == 'ho-eh-dor') return 1; 		// [
+	if (hodor == 'ho-ah-dor') return 1; 		// ]
 	if (hodor == 'hodor.') return 1; 		// .
-	if (hodor == 'ho-ah-dor?') return 1;	// ,
+	if (hodor == 'ho-ah-dor?') return 1;		// ,
 	if (hodor == 'hodoor!') return 1; 		// #
 	return 0;
 }
